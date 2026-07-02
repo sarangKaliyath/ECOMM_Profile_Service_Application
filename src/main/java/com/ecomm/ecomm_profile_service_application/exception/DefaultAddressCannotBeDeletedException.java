9205095ddac839795d80a750781de8a439f43c66
Sarang <sarang1221@gmail.com>
2026-07-02 12:55:26 +0530

    Change for jwtAuth, and Controller methods

---
+++ src/main/java/com/ecomm/ecomm_profile_service_application/exception/DefaultAddressCannotBeDeletedException.java
@@ -0,0 +1,7 @@
+package com.ecomm.ecomm_profile_service_application.exception;
+
+public class DefaultAddressCannotBeDeletedException extends RuntimeException {
+  public DefaultAddressCannotBeDeletedException(String message) {
+    super(message);
+  }
+}
